bd090b5c7b1902b294d12405c6f236bef560d1b8
WDW <aw13dw5450@gmail.com>
2015-10-25 20:17:07 +0900

    upload

--- 2d_code/main_state.py
+++ 2d_code/main_state.py
@@ -4,11 +4,43 @@ from pico2d import *
 
 import game_framework
 
+backgraound_width = 1600
+backgraound_height = 1800
+
+canvas_width = 800
+canvas_height = 600
+
 boy = None
 grass = None
 font = None
 running = True
 frame_time = 0
+
+class Background:
+    def __init__(self):
+        self.image = load_image('map.png')
+        self.left = 0;
+        self.bottom = 0;
+    def set_center_object(self, boy):
+        self.center_object = boy
+    def draw(self):
+        self.image.clip_draw_to_origin(self.left, self.bottom, int( backgraound_width - self.left * backgraound_width / 236),int( backgraound_height - self.bottom * backgraound_height / 270), 0, 0)
+
+    def update(self, frame_time):
+        if(int(boy.x) > canvas_width/2 and boy.dir == 1 and int(boy.x) < (backgraound_width - canvas_width)):
+            self.left +=1
+
+        if(int(boy.x) > canvas_width/2 and boy.dir == -1 and int(boy.x) < (backgraound_width - canvas_width)):
+            self.left -=1
+
+        if(int(boy.y) > canvas_height/2 and boy.dir_y == 1 and int(boy.y) < (backgraound_height - canvas_height)):
+            self.bottom +=1
+
+        if(int(boy.y) > canvas_height/2 and boy.dir_y == -1 and int(boy.y) < (backgraound_height - canvas_height)):
+            self.bottom -=1
+
+
+
 
 class Grass:
     def __init__(self):
@@ -41,9 +73,11 @@ class Boy:
     CAN_XMOVE = 4;              #4가능 0 불가능
     def __init__(self):
         self.x, self.y = 0, 90
+        self.draw_x, self.draw_y = None, None
         self.frame = random.randint(0, 7)
         self.total_frames = 0.0
         self.dir = 0
+        self.dir_y = 0;
         self.jup_max_point =  self.y + self.JUMP_HIGHT
         self.jump_dir = 0;
         self.jump_prev_state = 0;
@@ -61,17 +95,18 @@ class Boy:
         self.frame = int(self.total_frames) % 4
         self.x += (self.dir * distance)
         self.y += (self.jump_dir * distance)
+        self.y += (self.dir_y * distance)
 
-        if self.x > 800:
+        if self.x > backgraound_width - (canvas_width/2):
             self.dir = 0
-            self.x = 800
+            self.x = backgraound_width - (canvas_width/2)
             #self.state = self.LEFT_RUN
-            print("Change Time: %f, Total Frames: %d" % (get_time(), self.total_frames))
+            #print("Change Time: %f, Total Frames: %d" % (get_time(), self.total_frames))
         elif self.x < 0:
             self.dir = 0
             self.x = 0
             #self.state = self.RIGHT_RUN
-            print("Change Time: %f, Total Frames: %d" % (get_time(), self.total_frames))
+            #print("Change Time: %f, Total Frames: %d" % (get_time(), self.total_frames))
 
         if (self.jump_dir > 0 and self.y > self.jup_max_point):
             self.jump_dir = -1
@@ -81,11 +116,23 @@ class Boy:
             self.y = 90
             self.state = self.jump_prev_state
 
+        if(self.x < canvas_width /2 and self.x < canvas_width/2):
+            self.draw_x = self.x
+        elif(self.x >= backgraound_width /2  and self.x < (backgraound_width - canvas_width)):
+            self.draw_x = canvas_width/2
+        elif(self.x >=  (backgraound_width - canvas_width)):
+            self.draw_x = self.x - (backgraound_width - canvas_width) + canvas_width/2
+
+        if (self.y < canvas_height/2):
+            self.draw_y = self.y
+        elif(self.y >=  canvas_height/2 and self.y < (backgraound_height - canvas_height)):
+            self.draw_y = canvas_height/2
+        elif(self.y >=  (backgraound_height - canvas_height)):
+            self.draw_y = self.y - (backgraound_height - canvas_height) + canvas_height/2
 
     def draw(self):
         # fill here
-        self.image.clip_draw((self.frame + self.CAN_XMOVE) * 100, self.state * 100, 100, 100, self.x, self.y)
-
+         self.image.clip_draw((self.frame + self.CAN_XMOVE) * 100, self.state * 100, 100, 100,self.draw_x, self.draw_y)
 
     def handle_events(self, event, frame_time):
         if(event.type, event.key) == (SDL_KEYDOWN, SDLK_RIGHT):
@@ -129,14 +176,26 @@ class Boy:
                 self.state = self.LEFT_STAND
             self.CAN_XMOVE = 4
 
+        elif(event.type, event.key) == (SDL_KEYDOWN, SDLK_q):
+            self.dir_y = 1;
+        elif(event.type, event.key) == (SDL_KEYUP, SDLK_q):
+            self.dir_y = 0;
+
+        elif(event.type, event.key) == (SDL_KEYDOWN, SDLK_e):
+            self.dir_y = -1;
+        elif(event.type, event.key) == (SDL_KEYUP, SDLK_e):
+            self.dir_y = 0;
+
 
 
 def enter():
     # fill here
     open_canvas()
-    global boy, grass
+    global boy, grass, map
     boy = Boy()
     grass = Grass()
+    map = Background()
+    map.set_center_object(boy)
 
 
 
@@ -144,7 +203,8 @@ def exit():
     global boy, grass
     del(boy)
     del(grass)
-    #close_canvas()
+    del(map)
+    close_canvas()
 
 
 def pause():
@@ -167,11 +227,13 @@ def update(frame_time):
     # fill here
     frame_time+=0.01
     boy.update(frame_time)
+    map.update(frame_time)
     delay(0.01);
 
 def draw(frame_time):
     # fill here
     clear_canvas()
+    map.draw()
     grass.draw()
     boy.draw()
     update_canvas()
--- 2d_code/my_game.py
+++ 2d_code/my_game.py
@@ -2,7 +2,9 @@ import  game_framework
 
 import main_state
 
-game_framework.run(main_state)
+import map_tool
+
+game_framework.run(map_tool)
 
 # fill here
 
